851b21b852229f815814cab6fdd30f562fc2fb11
pornananMobil <86395604+pornananMobil@users.noreply.github.com>
2022-09-21 11:20:12 +0700

    Create HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
